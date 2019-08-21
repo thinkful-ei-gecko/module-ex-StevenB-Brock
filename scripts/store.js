b82b2adb7cc3dea7e5580c06748662468eddd002
Steven Bull <stevenwbulljr@gmail.com>
2019-08-21 14:05:00 -0500

    refactored to three global variables and completed assignment

--- scripts/store.js
+++ scripts/store.js
@@ -40,6 +40,14 @@ const store = (function () {
     this.items.splice(deleteThis, 1);
   };
 
+  const toggleCheckedFilter = function() {
+    this.hideCheckedItems = !this.hideCheckedItems;
+  };
+
+  const setSearchTerm = function(newSearchTerm) {
+    this.searchTerm = newSearchTerm;
+  };
+
   return {
     items,
     hideCheckedItems,
@@ -48,7 +56,9 @@ const store = (function () {
     addItem,
     findAndToggleChecked,
     findAndUpdateName,
-    findAndDelete
+    findAndDelete,
+    toggleCheckedFilter,
+    setSearchTerm
   };
 
 }() );
