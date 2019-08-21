b7d3a5eb25f3b0fd85eb73d65b13bec904189d39
Steven Bull <stevenwbulljr@gmail.com>
2019-08-21 13:51:38 -0500

    refactored store to include all methods required

--- scripts/store.js
+++ scripts/store.js
@@ -1,14 +1,54 @@
 'use strict';
+/* global store, cuid, Item */
+
+// eslint-disable-next-line no-unused-vars
 
 const store = (function () {
   const items = [];
   let hideCheckedItems = false;
   let searchTerm = '';
 
+  const findByID = function(id) {
+    return store.items.find(item => item.id === id);
+  };
+
+  const addItem = function(name) {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(e) {
+      console.log(e.message);
+    }
+  };
+
+  const findAndToggleChecked = function(id) {
+    const toggleThis = this.findByID(id);
+    toggleThis.checked = !toggleThis.checked;
+  };
+
+  const findAndUpdateName = function(id, newName) {
+    try {
+      Item.validateName(newName);
+      findByID(id).name = newName;
+    } catch(e) {
+      console.log(e.message);
+    }
+  };
+
+  const findAndDelete = function(id) {
+    const deleteThis = this.items.findIndex(item => item.id === id);
+    this.items.splice(deleteThis, 1);
+  };
+
   return {
     items,
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    findByID,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   };
 
 }() );
